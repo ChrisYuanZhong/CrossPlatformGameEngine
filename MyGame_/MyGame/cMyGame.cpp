fd16ca0e2fe0c8c66e1bd2766a3d43bec0e60e23
Chris Yuan Zhong <chrisyuanzhong@outlook.com>
2023-08-29 18:11:35 -0600

    Updated log content and build dependency for MyGame project

--- MyGame_/MyGame/cMyGame.cpp
+++ MyGame_/MyGame/cMyGame.cpp
@@ -31,12 +31,12 @@ void eae6320::cMyGame::UpdateBasedOnInput()
 
 eae6320::cResult eae6320::cMyGame::Initialize()
 {
-	eae6320::Logging::OutputMessage("Initializing the application");
+	eae6320::Logging::OutputMessage("Initializing Chris Yuan Zhong's EAE6320 Example Game");
 	return Results::Success;
 }
 
 eae6320::cResult eae6320::cMyGame::CleanUp()
 {
-	eae6320::Logging::OutputMessage("Cleaning up the application");
+	eae6320::Logging::OutputMessage("Cleaning up Chris Yuan Zhong's EAE6320 Example Game");
 	return Results::Success;
 }
