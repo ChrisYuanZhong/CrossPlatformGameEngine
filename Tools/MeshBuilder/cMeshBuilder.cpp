e79419c4081aa7cb8e9ecfbed4e145c77859cb3d
Chris Yuan Zhong <chrisyuanzhong@outlook.com>
2023-10-17 01:39:26 -0600

    MeshBuilder done

---
+++ Tools/MeshBuilder/cMeshBuilder.cpp
@@ -0,0 +1,27 @@
+// Includes
+//=========
+
+#include "cMeshBuilder.h"
+
+#include <Tools/AssetBuildLibrary/Functions.h>
+#include <Engine/Platform/Platform.h>
+
+// Inherited Implementation
+//=========================
+
+// Build
+//------
+
+eae6320::cResult eae6320::Assets::cMeshBuilder::Build(const std::vector<std::string>& i_arguments)
+{
+	//Copy the source file to the target file
+	{
+		std::string errorMessage;
+		if (!Platform::CopyFile(m_path_source, m_path_target, &errorMessage))
+		{
+			OutputErrorMessageWithFileInfo(m_path_source, errorMessage.c_str());
+			return Results::Failure;
+		}
+		return Results::Success;
+	}
+}
